a231f53e15af68852a5f511c1ec3fc8d843e69aa
Hoqk4baZ <115368470+hoqk4baz@users.noreply.github.com>
2023-09-18 01:37:25 +0300

    Update cark.py

--- cark.py
+++ cark.py
@@ -1,32 +1,44 @@
-from flask import Flask, jsonify
+from flask import Flask, jsonify, request
+from flask_sslify import SSLify
+
 
 app = Flask(__name__)
+sslify = SSLify(app)
 
-@app.route('/gamification/getSquatMarketingProductStatus', methods=['GET'])
-def get_status():
-    response_data = {
-        "success": True,
-        "code": 202,
-        "message": "Eligible",
-        "isJourneyComplete": True,
-        "wheelConfig": {
-            "isYouth": True,
-            "isBirthday": False,
-            "isAnniversary": False,
-            "wheelSlices": [
-                {"name": "İnternet", "value": "1"},
-                {"name": "Dakika", "value": "2"},
-                {"name": "Çekiliş Yanımda", "value": "3"},
-                {"name": "İnternet", "value": "4"},
-                {"name": "Her Şey Yanımda", "value": "5"},
-                {"name": "İndirim Çeki", "value": "6"},
-                {"name": "İnternet", "value": "7"},
-                {"name": "Sınırsız Paket", "value": "8"}
-            ]
-        },
-        "isTermsAndConditionsRequired": False
-    }
-    return jsonify(response_data)
+@app.route('/', defaults={'path': ''}, methods=['CONNECT','GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'])
+@app.route('/<path:path>', methods=['CONNECT','GET', 'POST', 'PUT', 'DELETE', 'PATCH', 'OPTIONS'])
+def catch_all(path):
+    # İstekin yapıldığı URL'yi alın
+    request_url = request.url
+    
+    # Eğer istek URL'si /gamification/getSquatMarketingProductStatus içeriyorsa JSON yanıtını döndürün
+    if '/gamification/getSquatMarketingProductStatus' in request_url:
+        response_data = {
+            "success": True,
+            "code": 202,
+            "message": "Eligible",
+            "isJourneyComplete": True,
+            "wheelConfig": {
+                "isYouth": True,
+                "isBirthday": False,
+                "isAnniversary": False,
+                "wheelSlices": [
+                    {"name": "İnternet", "value": "1"},
+                    {"name": "Dakika", "value": "2"},
+                    {"name": "Çekiliş Yanımda", "value": "3"},
+                    {"name": "İnternet", "value": "4"},
+                    {"name": "Her Şey Yanımda", "value": "5"},
+                    {"name": "İndirim Çeki", "value": "6"},
+                    {"name": "İnternet", "value": "7"},
+                    {"name": "Sınırsız Paket", "value": "8"}
+                ]
+            },
+            "isTermsAndConditionsRequired": False
+        }
+        return jsonify(response_data)
+    
+    # Diğer URL'ler için hiçbir şey yapmayın (engelleme veya hata verme)
+    return ""
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=3131)
